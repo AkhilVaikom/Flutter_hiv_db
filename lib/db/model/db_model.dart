ab29a3fa0336e2ee9ad2e10be92f5d144c24b2a4
AkhilVaikom <akhilvkm1991@gmail.com>
2021-11-25 22:31:54 +0530

    Add db

--- lib/db/model/db_model.dart
+++ lib/db/model/db_model.dart
@@ -1,5 +1,13 @@
+import 'package:hive_flutter/adapters.dart';
+part 'db_model.g.dart';
+
+@HiveType(typeId: 1)
+
 class DataModel{
+  @HiveField(0)
   final String name;
+
+  @HiveField(1)
   final String age;
 
   DataModel({required this.name,required this.age});
